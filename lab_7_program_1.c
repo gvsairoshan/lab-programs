e4a60b3085e32fab26798814518a3323b42f3366
gvsairoshan <74449502+gvsairoshan@users.noreply.github.com>
2021-04-28 16:24:16 +0530

    Add files via upload

---
+++ lab_7_program_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c",&ch);
+    printf("ASCII value of the given character is %d",ch);
+
+    return 0;
+}
